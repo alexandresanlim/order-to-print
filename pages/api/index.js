1d58eb427d4ac1d57347cd4ea9eee058ef1c55d8
Alexandre Lima <alexandreslima@outlook.com>
2020-10-04 16:13:43 -0300

    test title

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,7 +4,7 @@ export default (req, res) => {
   const text = req.query.text;
 
 
-  var textHtml = "<html><head><meta charset='utf-8'><meta name='description' content='Uma nova descrição'><title>Novo Pedido!</title></head> <body>"+text+"</body> </html>";
+  var textHtml = "<html><head><meta charset='utf-8'><meta name='description' content='Uma nova descrição'><title>"+text+"</title></head> <body>"+text+"</body> </html>";
 
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/html')
