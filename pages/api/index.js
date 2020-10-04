06c88897a153e5cd2946e9606df61c3a3a0c65d3
Alexandre Lima <alexandreslima@outlook.com>
2020-10-04 15:51:17 -0300

    new text

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,11 @@ export default (req, res) => {
 
   const text = req.query.text;
 
+
+  var textHtml = 
+  "<html><title>Novo Pedido!</title> <body>"+text+"</body> </html>"
+
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/html')
-  res.send('<h1>'+text+'</h1>')
+  res.send(textHtml)
 }
