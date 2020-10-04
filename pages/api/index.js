3804894efe6f400b476b034721ebb3829cc8ccec
Alexandre Lima <alexandreslima@outlook.com>
2020-10-04 15:44:22 -0300

    nex

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,6 +4,6 @@ export default (req, res) => {
   const text = req.query.text;
 
   res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain')
-  res.send(text)
+  res.setHeader('Content-type', 'text/html')
+  res.send('<h1>'+text+'</h1>')
 }
--- pages/index.js
+++ pages/index.js
@@ -3,3 +3,7 @@ const Index = () =>{
 }
 
 export default Index
+
+
+
+
