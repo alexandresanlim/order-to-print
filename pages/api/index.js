d7effe28e1251385412415b284e8a5bf1f5dd7ff
Alexandre Lima <alexandreslima@outlook.com>
2020-11-17 12:51:14 -0300

    new features

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,10 +1,24 @@
 
 export default (req, res) => {
 
-  const text = req.query.text;
+  const body = req.query.body;
+  const title = req.query.title;
+  const number = req.query.number;
+  const total = req.query.total;
 
 
-  var textHtml = "<html><head><meta charset='utf-8'><meta name='description' content='Uma nova descrição'><title>"+text+"</title></head> <body>"+text+"</body> </html>";
+  var textHtml = "<html>"+
+  "<head><meta charset='utf-8'><meta name='"+body+"' content='"+body+"'><title>"+title+"</title></head> "+
+  "<body>"+
+    " <b>"+title+"</b> <br/>"+
+    "............................................. <br/>"+
+    "Number: "+number+" <br/>"+
+    "............................................. <br/>"+
+    " "+body+" <br/>"+
+    "............................................. <br/>"+
+    "TOTAL: "+total+" "+
+  "</body>"+
+  "</html>";
 
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/html')
